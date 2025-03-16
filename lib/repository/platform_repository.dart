113f7cb77bb9371ccbc607343ac55ebc116a470a
Karol Rakicki <rakichuck@gmail.com>
2025-03-16 12:33:27 +0100

    added snackbar info and progressIndicator

--- lib/repository/platform_repository.dart
+++ lib/repository/platform_repository.dart
@@ -1,16 +1,47 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 class PlatformRepository {
   static const platform = MethodChannel('flutter.native/sensor/imperative');
 
-  Future<void> connectToDevice(String mac) async {
+  bool isLoading = false;
+  showLoading(BuildContext context) {
+    isLoading = true;
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) {
+        return const Center(child: CircularProgressIndicator());
+      },
+    );
+  }
+
+  hideLoading(BuildContext context) {
+    if (!isLoading) return;
+
+    isLoading = false;
+    Navigator.of(context, rootNavigator: true).pop();
+  }
+
+  Future<void> connectToDevice(BuildContext context, String mac) async {
+    showLoading(context);
     try {
-      final String result = await platform.invokeMethod('connectToSensorService', {'macAddress': mac});
+      final String result = await platform.invokeMethod(
+        'connectToSensorService',
+        {'macAddress': mac},
+      );
       print(result); // "Connected to <mac>"
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text('Connected successfully to $mac')));
     } on PlatformException catch (e) {
       // @TODO handle error during connection
       print("Failed to connect: ${e.message}");
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Failed to connect: ${e.message}')),
+      );
     }
+    hideLoading(context);
   }
 
   Future<List<String>?> getSensorData() async {
