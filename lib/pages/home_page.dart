11846195a9c580eb4c0a3654278936a82a557127
Karol Rakicki <rakichuck@gmail.com>
2025-04-11 21:19:05 +0200

    added text box for inputing mac

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -15,6 +15,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   final _repository = PlatformRepository();
   String sensorData = "N/A";
+  TextEditingController _controller = TextEditingController();
 
   connect(String mac) async {
     await _repository.connectToDevice(context, mac);
@@ -23,17 +24,25 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue,
+      //  backgroundColor: Colors.blue,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            TextFormField(
+              controller: _controller,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Enter a mac address of divice',
+              ),
+            ),
             ElevatedButton(
               onPressed: () {
                 setState(() {
                   // @TODO, mac address need to be configurable
-                  connect(getMacAddress(1));
+                  //connect(getMacAddress(1));
                   // connect("C1:74:71:F3:94:E0");
+                  connect(_controller.text);
                   Timer.periodic(Duration(seconds: 2), (timer) async {
                     List<String>? data = await _repository.getSensorData();
                     if (data != null && data.isNotEmpty) {
