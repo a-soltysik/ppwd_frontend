5e77641de69eb6d3bcc8b43c261cebcd23174466
a-soltysik <adzi11235@gmail.com>
2025-04-12 16:27:38 +0200

    Add backend integration

--- lib/services/sensor_service.dart
+++ lib/services/sensor_service.dart
@@ -2,27 +2,29 @@ import 'dart:convert';
 import 'dart:developer';
 
 import 'package:http/http.dart' as http;
-import 'package:ppwd_frontend/utils.dart';
 
 import '../repository/platform_repository.dart';
 
 class SensorService {
+  static const String apiKey = 'apikey';
+
   Future<void> sendSensorData(Board boardData) async {
-    final Uri url = Uri.parse('$apiUrl/send/data');
+    final Uri url = Uri.parse('http://156.17.41.203:55555/api/measurements');
 
     try {
       final http.Response response = await http.post(
         url,
-        headers: {'Content-Type': 'application/json'},
+        headers: {'Content-Type': 'application/json', 'X-Api-Key': apiKey},
         body: jsonEncode(boardData),
       );
       log(jsonEncode(boardData));
 
-      if (response.statusCode == 200) {
+      if (response.statusCode == 201) {
         print('Data sent successfully');
       } else {
         print('Error: ${response.statusCode}');
       }
+      print('Response body: ${response.body}');
     } catch (e) {
       print('Exception: $e');
     }
