bf05f1285f634025f70ab19ee6af547976d0b65c
Karol Rakicki <rakichuck@gmail.com>
2025-04-16 23:45:05 +0200

    added suggest enterd bluetooth

--- lib/core/utils/mac_address_utils.dart
+++ lib/core/utils/mac_address_utils.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class MacAddressInputFormatter extends TextInputFormatter {
   @override
@@ -78,7 +79,7 @@ class MacAddressInputFormatter extends TextInputFormatter {
   }
 }
 
-class MacAddressTextField extends StatelessWidget {
+class MacAddressTextField extends StatefulWidget {
   final TextEditingController controller;
   final bool enabled;
 
@@ -89,24 +90,146 @@ class MacAddressTextField extends StatelessWidget {
   });
 
   @override
-  Widget build(BuildContext context) {
-    return TextFormField(
-      controller: controller,
-      decoration: const InputDecoration(
-        border: OutlineInputBorder(),
-        labelText: 'Enter a MAC address of device',
-        hintText: 'Format: XX:XX:XX:XX:XX:XX',
-        prefixIcon: Icon(Icons.bluetooth),
-        helperText: 'Colons will be added automatically',
-      ),
-      enabled: enabled,
-      inputFormatters: [
-        MacAddressInputFormatter(),
-        LengthLimitingTextInputFormatter(17), // XX:XX:XX:XX:XX:XX = 17 chars
-      ],
-      textCapitalization: TextCapitalization.characters,
-      autocorrect: false,
-      enableSuggestions: false,
+  State<MacAddressTextField> createState() => _MacAddressTextFieldState();
+}
+
+class _MacAddressTextFieldState extends State<MacAddressTextField> {
+  final FocusNode _focusNode = FocusNode();
+  final LayerLink _layerLink = LayerLink();
+  OverlayEntry? _overlayEntry;
+  List<String> _macSuggestions = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadMacSuggestions();
+    _focusNode.addListener(() {
+      if (_focusNode.hasFocus) {
+        _showOverlay();
+      } else {
+        _removeOverlay();
+      }
+    });
+  }
+
+  Future<void> _loadMacSuggestions() async {
+    final prefs = await SharedPreferences.getInstance();
+    _macSuggestions = prefs.getStringList('mac_addresses') ?? [];
+    if (mounted) setState(() {});
+  }
+
+  Future<void> _saveMacAddress(String mac) async {
+    final prefs = await SharedPreferences.getInstance();
+    final List<String> current = prefs.getStringList('mac_addresses') ?? [];
+    if (!current.contains(mac)) {
+      current.add(mac);
+      await prefs.setStringList('mac_addresses', current);
+      _loadMacSuggestions();
+    }
+  }
+
+  Future<void> _deleteMacAddress(String mac) async {
+    final prefs = await SharedPreferences.getInstance();
+    final List<String> current = prefs.getStringList('mac_addresses') ?? [];
+    current.remove(mac);
+    await prefs.setStringList('mac_addresses', current);
+    _loadMacSuggestions();
+  }
+
+  void _showOverlay() {
+    _overlayEntry = _createOverlayEntry();
+    Overlay.of(context).insert(_overlayEntry!);
+  }
+
+  void _removeOverlay() {
+    _overlayEntry?.remove();
+    _overlayEntry = null;
+  }
+
+  OverlayEntry _createOverlayEntry() {
+    RenderBox renderBox = context.findRenderObject() as RenderBox;
+    Size size = renderBox.size;
+    Offset offset = renderBox.localToGlobal(Offset.zero);
+
+    return OverlayEntry(
+      builder:
+          (context) => Positioned(
+            width: size.width,
+            child: CompositedTransformFollower(
+              link: _layerLink,
+              showWhenUnlinked: false,
+              offset: Offset(0.0, size.height + 8.0),
+              child: Material(
+                elevation: 4.0,
+                child: ListView(
+                  padding: EdgeInsets.zero,
+                  shrinkWrap: true,
+                  children:
+                      _macSuggestions.map((mac) {
+                        return Dismissible(
+                          key: Key(mac),
+                          direction: DismissDirection.endToStart,
+                          background: Container(
+                            color: Colors.red,
+                            alignment: Alignment.centerRight,
+                            padding: const EdgeInsets.symmetric(horizontal: 20),
+                            child: const Icon(
+                              Icons.delete,
+                              color: Colors.white,
+                            ),
+                          ),
+                          onDismissed: (_) => _deleteMacAddress(mac),
+                          child: ListTile(
+                            title: Text(mac),
+                            onTap: () {
+                              widget.controller.text = mac;
+                              _focusNode.unfocus();
+                              _removeOverlay();
+                            },
+                          ),
+                        );
+                      }).toList(),
+                ),
+              ),
+            ),
+          ),
     );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CompositedTransformTarget(
+      link: _layerLink,
+      child: TextFormField(
+        controller: widget.controller,
+        focusNode: _focusNode,
+        decoration: const InputDecoration(
+          border: OutlineInputBorder(),
+          labelText: 'Enter a MAC address of device',
+          hintText: 'Format: XX:XX:XX:XX:XX:XX',
+          prefixIcon: Icon(Icons.bluetooth),
+          helperText: 'Colons will be added automatically',
+        ),
+        enabled: widget.enabled,
+        inputFormatters: [
+          MacAddressInputFormatter(),
+          LengthLimitingTextInputFormatter(17),
+        ],
+        textCapitalization: TextCapitalization.characters,
+        autocorrect: false,
+        enableSuggestions: false,
+        onFieldSubmitted: (value) async {
+          await _saveMacAddress(value.toUpperCase());
+          _focusNode.unfocus();
+        },
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _focusNode.dispose();
+    _removeOverlay();
+    super.dispose();
   }
 }
